a6541e0998520db992ed400c35167e4f237f37ff
Pratik <cpratik025@gmail.com>
2019-10-05 17:26:56 -0400

    1

---
+++ Jenkins.py
@@ -0,0 +1,16 @@
+# !/bin/python3
+
+import jenkins
+from time import sleep
+
+
+token = '118c8df204629792403c6898be4c42ef0b'
+
+
+def jenkins_conn():
+    print("connecting to Jenkins...")
+    sleep(2)
+
+    jconn = jenkins.Jenkins('localhost:8080', username='pratik025', password=token)
+    version = jconn.get_version()
+    print('Hello from Jenkins {}'.format(version))
