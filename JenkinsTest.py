a42129a21142c404efb4d7881990f1ff554f7265
Pratik <cpratik025@gmail.com>
2019-10-13 05:00:18 -0400

    Update JenkinsTest.py

--- JenkinsTest.py
+++ JenkinsTest.py
@@ -4,13 +4,13 @@ import time
 
 
 
-job_name = "testjob"   #Give your job name here
+job_name = "Groovy"   #Give your job name here
 
 
 def jenkins_job_status(job_name):
         
         try:
-                url  = "https://your_jenkins_endpoint/job/%s/lastBuild/api/json" %job_name   #Replace 'your_jenkins_endpoint' with your Jenkins URL
+                url  = "http://localhost:8080/job/%s/lastBuild/api/json" %job_name   #Replace 'your_jenkins_endpoint' with your Jenkins URL
                 while True:
                         data = requests.get(url).json()
                         if data['building']:
