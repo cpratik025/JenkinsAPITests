5e97d91d93f43ee53de99572c78f5a7fdb6f08f6
Pratik <cpratik025@gmail.com>
2019-10-13 01:54:37 -0400

    Update Jenkins.py

--- Jenkins.py
+++ Jenkins.py
@@ -1,18 +1,23 @@
-# !/bin/python3
+server.create_node('slave1')
+nodes = get_nodes()
+print nodes
+node_config = server.get_node_info('slave1')
+print node_config
+server.disable_node('slave1')
+server.enable_node('slave1')
 
-from jenkinsapi.jenkins import Jenkins
-from time import sleep
-
-
-token = '118c8df204629792403c6898be4c42ef0b'
-
-
-def jenkins_conn():
-    print("connecting to Jenkins...")
-    sleep(2)
-
-    jconn = Jenkins.Jenkins('http://192.168.137.1:8080', username='pratik025', password=token)
-    version = jconn.get_version()
-    print('Hello from Jenkins {}'.format(version))
-
-jenkins_conn()
+# create node with parameters
+params = {
+    'port': '22',
+    'username': 'juser',
+    'credentialsId': '10f3a3c8-be35-327e-b60b-a3e5edb0e45f',
+    'host': 'my.jenkins.slave1'
+}
+server.create_node(
+    'slave1',
+    nodeDescription='my test slave',
+    remoteFS='/home/juser',
+    labels='precise',
+    exclusive=True,
+    launcher=jenkins.LAUNCHER_SSH,
+    launcher_params=params)
